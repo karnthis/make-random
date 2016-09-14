0cbbb90a9a021b5db10562a5c7296aded764c6ad
John Foderaro <johnfoderaro@icloud.com>
2016-09-14 02:50:55 +0000

    removing example.js

--- example.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const makeRandom = require('./index.js');
-
-// console.log(makeRandom.floor(-10));
-// console.log(makeRandom.ceil(-0));
-// console.log(makeRandom.floor(10));
-// console.log(makeRandom.ceil(10));
