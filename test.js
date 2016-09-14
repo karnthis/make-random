cffeaab7bbc8934d407791903a69037ba401d333
John Foderaro <johnfoderaro@icloud.com>
2016-09-14 02:44:50 +0000

    completing unit tests

---
+++ test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const makeRandom = require('./index');
+
+describe('makeRandom', () => {
+  it(`should have the methods 'ceil' and 'floor'`, ()=> {
+    expect(makeRandom.ceil).to.be.a('function');
+    expect(makeRandom.floor).to.be.a('function');
+  });
+  it(`should return a number`, () => {
+    expect(makeRandom.ceil(10)).to.be.a('number');
+    expect(makeRandom.floor(10)).to.be.a('number');
+  });
+  it(`should not return a negative zero`, () => {
+    expect(makeRandom.ceil(-0)).to.equal(0);
+  });
+  it(`should throw an error if a number is not passed`, () => {
+    expect(() => {
+      makeRandom.ceil('test');
+    }).to.throw(Error);
+    expect(() => {
+      makeRandom.ceil('10');
+    }).to.throw(Error);
+    expect(() => {
+      makeRandom.ceil(true);
+    }).to.throw(Error);
+    expect(() => {
+      makeRandom.ceil({});
+    }).to.throw(Error);
+  });
+});
