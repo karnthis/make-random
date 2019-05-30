de366ee0c29557fc1f153d1f5a2c666d47330110
Erin <erin@atomicbear.com>
2019-05-30 06:01:50 -0500

    reworking structure to streamline tests and deprecate old

--- index.js
+++ index.js
@@ -3,22 +3,9 @@
 const Purify = require('purify-int')
 const Crypto = require('crypto')
 
-const makeRandom = {
-	ceil(max) {
-		const safeMax = Purify.asInt(max);
-		deprecate("make-random.ceil()", "0.1.6");
-		return Math.ceil(Math.random() * safeMax);
-	},
-	floor(max) {
-		const safeMax = Purify.asInt(max);
-		deprecate("make-random.floor()", "0.1.6");
-		return Math.floor(Math.random() * safeMax);
-	}
-};
+const { floor, ceil } = require('./deprecated/v1')
 
-function deprecate(func, version) {
-	console.log(`${func} is deprecated as of ${version} and may be removed at any time.
-	Please convert to make-random-legacy if this function is still needed.`)
+module.exports = {
+	floor,
+	ceil
 }
-
-module.exports = makeRandom;
