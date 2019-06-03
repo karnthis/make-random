db4426fb8d512d9e56695bafc0c0a50ec4c28e5b
Erin <erin@atomicbear.com>
2019-06-03 05:12:07 -0500

    correcting some tests, finishing setRange() + tests

--- libs/index.js
+++ libs/index.js
@@ -3,19 +3,65 @@
 const Purify = require('purify-int')
 const Crypto = require('crypto')
 
+const debug = true
+
 // INTERNAL
 function foundation(base) {
-	const pureBase = Math.abs(base)
-	const byteCount = Math.ceil(pureBase / 256)
+	const trueBase = Math.abs(base)
+	const byteCount = makeByteCount(trueBase)
+	// if (debug) console.log(byteCount)
 	const hexString = Crypto.randomBytes(byteCount).toString('hex')
-	return parseInt(hexString, 16) % pureBase
+	// if (debug) console.log(parseInt(hexString, 16))
+	return parseInt(hexString, 16) % trueBase
+}
+
+function makeByteCount(max) {
+	let ret = 1
+	let cycle = 256
+	while (max > cycle) {
+		cycle *= 256
+		ret++
+	}
+	return ret
+}
+
+function offsetNum(v, invert = false) {
+	if (!invert) {
+		return (v > 0) ? ++v : --v
+	} else {
+		return (v < 0) ? ++v : --v
+	}
+	
 }
 
 // EXTERNAL
-function flexRange(v) {
-	const pureV = Purify.asInt(v)
+function flexRange(v = 1) {
+	v = (v > 0) ? ++v : --v
+	const pureV = Purify.asInt(v, 1)
 	const result = foundation(pureV)
-	return (pureV < 0) ? result * -1 : result
+	console.log(result)
+	return (pureV < 0 && result !== 0 ) ? result * -1 : result
 }
 
-module.exports = { flexRange }
+function setRange(v1 = -100, v2 = 100) {
+	v1 = offsetNum(Purify.asInt(v1, 1))
+	v2 = offsetNum(Purify.asInt(v2, 1))
+	let high, low
+	if (v1 < v2) {
+		high = v2
+		low = v1
+	} else if (v1 > v2) {
+		high = v1
+		low = v2
+	} else {
+		return flexRange(offsetNum(v2,true))
+	}
+	const baseValue = high - low
+	const result = foundation(baseValue)
+	return (baseValue < 0) ? (result * -1) + low : result + low
+}
+
+console.log(flexRange(-5))
+// console.log(setRange(20, 10))
+
+module.exports = { flexRange, setRange }
--- test/main/index.js
+++ test/main/index.js
@@ -1,25 +1,73 @@
 'use strict';
 
 const expect = require('chai').expect;
-const { fixedRange, flexRange } = require('../../libs');
+const { flexRange, setRange } = require('../../libs');
 
-describe('makeRandom flexRange()', () => {
-	it(`should have the method 'flexRange'`, ()=> {
+describe('flexRange()', () => {
+	it(`should be a function`, ()=> {
 		expect(flexRange).to.be.a('function');
 	});
 	it(`should return a positive random number`, () => {
-		expect(flexRange(10)).to.be.a('number').and.be.greaterThan(-1);
-		expect(flexRange('10')).to.be.an('number').and.be.greaterThan(-1);
+		expect(flexRange(10)).to.be.a('number').and.be.at.least(0);
+		expect(flexRange('10')).to.be.an('number').and.be.at.least(0);
 	});
 	it(`should return a negative random number`, () => {
-		expect(flexRange(-10)).to.be.a('number').and.be.lessThan(0);
-		expect(flexRange('-10')).to.be.an('number').and.be.lessThan(0);
+		expect(flexRange(-10)).to.be.a('number').and.to.be.at.most(0);
+		expect(flexRange('-10')).to.be.an('number').and.to.be.at.most(0);
+	});
+	it(`should return randomly 0 or 1 if passed no arguments`, () => {
+		expect(flexRange()).to.be.within(0,1);
+	});
+	it(`should return 0 if a number or number string is not passed to range()`, () => {
+		expect(flexRange('test')).to.equal(0);
+		// expect(flexRange(true)).to.equal(0);
+		expect(flexRange({})).to.equal(0);
+		expect(flexRange([])).to.equal(0);
+		expect(flexRange(() => {})).to.equal(0);
+	});
+});
+
+describe('setRange()', () => {
+	it(`should be a function`, ()=> {
+		expect(setRange).to.be.a('function');
+	});
+	// it(`should return a positive random number`, () => {
+	// 	expect(setRange(10)).to.be.a('number').and.be.greaterThan(-1);
+	// 	expect(setRange('10')).to.be.an('number').and.be.greaterThan(-1);
+	// });
+	// it(`should return a negative random number`, () => {
+	// 	expect(setRange(-10)).to.be.a('number').and.be.lessThan(0);
+	// 	expect(setRange('-10')).to.be.an('number').and.be.lessThan(0);
+	// });
+	it(`should return randomly -100 to 100 if passed no arguments`, () => {
+		expect(setRange()).to.be.an('number').to.be.within(-100,100);
+	});
+	it(`should return randomly 10 to 50 if passed those values`, () => {
+		expect(setRange(10,50)).to.be.an('number').to.be.within(10,50);
+	});
+	it(`should handle if order is flipped`, () => {
+		expect(setRange(50,10)).to.be.an('number').to.be.within(10,50);
+	});
+	it(`should handle mixed positive/negative numbers (-10 to 10)`, () => {
+		expect(setRange(-10,10)).to.be.an('number').to.be.within(-10,10);
+	});
+	it(`should handle negative numbers (-10 to -50)`, () => {
+		expect(setRange(-10,-50)).to.be.an('number').to.be.within(-50,-10);
+	});
+	it(`should handle numbers as strings ('10' to '-50')`, () => {
+		expect(setRange('10','-50')).to.be.an('number').to.be.within(-50,10);
+	});
+	it(`should handle mixed numbers as strings ('10' to -50)`, () => {
+		expect(setRange('10',-50)).to.be.an('number').to.be.within(-50,10);
+	});
+	it(`should default to flexRange() behavior if input is the same`, () => {
+		expect(setRange(10,10)).to.be.an('number').and.be.greaterThan(-1);
 	});
 	// it(`should return 0 if a number or number string is not passed to range()`, () => {
-	// 	expect(flexRange('test')).to.equal(0);
-	// 	expect(flexRange(true)).to.equal(0);
-	// 	expect(flexRange({})).to.equal(0);
-	// 	expect(flexRange([])).to.equal(0);
-	// 	expect(flexRange(() => {})).to.equal(0);
+	// 	expect(setRange('test')).to.equal(0);
+	// 	expect(setRange(true)).to.equal(0);
+	// 	expect(setRange({})).to.equal(0);
+	// 	expect(setRange([])).to.equal(0);
+	// 	expect(setRange(() => {})).to.equal(0);
 	// });
 });
